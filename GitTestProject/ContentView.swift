f7f1fda32d651b92e30d7966a4918a3e5ce003ca
jomaro110 <matophme@yahoo.com>
2024-05-14 00:50:06 +0300

    Initial Commit

---
+++ GitTestProject/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  GitTestProject
+//
+//  Created by Haidar Hakim on 14/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello World! This is a new line")
+            Text("Hello hello")
+        }
+        .padding()
+        .frame(width: 250, height: 100)
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitTestProject/GitTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestProjectApp.swift
+//  GitTestProject
+//
+//  Created by Haidar Hakim on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
